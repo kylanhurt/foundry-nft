68efa9bc27877929c4d9b8860aa8253dedd6e2a6
kylanhurt <kylan.hurt@gmail.com>
2024-08-20 12:53:54 -0500

    Add testing (one failing still)

--- test/integrations/BasicNftTest.t.sol
+++ test/integrations/BasicNftTest.t.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+
 import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/Script.sol";
-import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
-import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../../src/BasicNft.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
